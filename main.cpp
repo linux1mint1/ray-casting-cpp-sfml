54212c014f7d238754c94b040cf3007616f3396d
linux1mint1 <olilechnik@gmail.com>
2022-03-26 11:40:05 +0100

    nothing new

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML works!", sf::Style::Default, settings);
+    sf::CircleShape shape(300.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
